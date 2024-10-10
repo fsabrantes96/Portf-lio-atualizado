c9eddb8b27f98d25d2f170378f1d054c043f61c9
fsabrantes <fsabrantes96@gmail.com>
2024-10-10 12:10:26 -0300

    trabalhando com swiper

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ const el = document.querySelector("#text");
 const text = "Olá! Me chamo Felipe, bem-vindo(a) ao meu portfólio, espero que goste! 😃";
 const interval = 100;
 
+
+
 function showText(el, text, interval){
     const char = text.split("").reverse();
 
@@ -17,6 +19,40 @@ function showText(el, text, interval){
     }, interval)
 }
 
-
-
 showText(el, text, interval);
+
+var swiper = new Swiper(".slide-projetos", {
+    slidesPerView: 3.5,
+    spaceBetween: 1,
+    speed: 800,
+    pagination: {
+      el: "#projetos .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".projetos-container .btn-next",
+      prevEl: ".projetos-container .btn-prev",
+    },
+    breakpoints: {
+      350: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+      576: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+      720: {
+        slidesPerView: 2,
+        spaceBetween: 16,
+      },
+      900:{
+        slidesPerView: 2,
+        spaceBetween: 16,
+      },
+      1200: {
+        slidesPerView: 3.5,
+        spaceBetween: 1,
+      }
+    }
+  });
